ae26d06322721265bdce05ade49f30201b897d99
timurhab189 <timurhab189@mail.ru>
2025-02-04 09:53:12 +0500

    Добавьте файлы проекта.

---
+++ Khabibullin41/ProductPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Khabibullin41
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+            
+            var currentProducts = Khabibullin41Entities.getInstance().Product.ToList();
+
+            ProductListView.ItemsSource = currentProducts;
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+    }
+}
